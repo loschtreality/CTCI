9d44f4b3c1ad30f185bee7f0bfcd40df21e4359d
Michael Loschiavo <losch103@gmail.com>
2017-03-01 11:55:14 -0800

    add files for stacks_queues section

---
+++ Stacks_Queues/animal_shelter.js
@@ -0,0 +1,8 @@
+// An animal shelter, which holds only dogs and cats operates strictly on
+// a "first in first out" basis. People must adopt either the oldest
+// (based on arrival time) of all animals at the shelter, or they can select
+// weather they would prefer a dog or cat (and will receive the oldest animal of
+// that type). They cannot select which specific animal they would like. Create
+// the data structures to maintain this system and implement operations such as
+// enqueue, dequeueAny, dequeueDog, dequeueCat. You may use the built-in
+// LinkedList data structure
---
+++ Stacks_Queues/stack_of_plates.js
@@ -0,0 +1,12 @@
+// Imagine a (literal) stack of plates. If the stack gets too high,
+// it might topple. Therefore, in real life, we would likely start
+// a new stack when the previous stack exceeds some threshold. Implement
+// a data structre SetOfStacks that mimics this. SetOfStacks should be
+// composed of several stacks and should create anew stac once the previous
+// once exceeds capaciy. SetOfStacks.push() and SetOfStacks.pop() should behave
+// identically to a single stack (that is, pop() should return the same values
+// as if there were just a single stack)
+
+// Follow up:
+// Implement a funciton popAt(index) which performs a pop operation on a
+// specific sub-stack
