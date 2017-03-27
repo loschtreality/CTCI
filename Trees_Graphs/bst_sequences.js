18be3c1d3a26cf5430765ce675b734cc41bb64b1
Michael Loschiavo <losch103@gmail.com>
2017-03-27 11:00:43 -0700

    add bst_sequences & paths_with_sum, stuck on both

--- Trees_Graphs/bst_sequences.js
+++ Trees_Graphs/bst_sequences.js
@@ -11,3 +11,72 @@
         // 1       3
 
 // Output: {2, 1, 3}, {2, 3, 1}
+
+// Clarifications:
+
+// Ideas:
+  // The two printings in the example are modificatons of pre-order traversal.
+  // In order to get other printings, the other traversing methods can be used.
+  // This incluces dfs, bfs, post-order, in-order. The nodes can go in the order
+  // which are natural to the serching methods (left then right) or unaturally
+
+  // Generate permutations of node combinations. Print the permutations -.-
+
+const { bstWithParent } = require('./graph');
+
+const bstSequences = (root) => {
+  if (!root) {
+    return null
+  }
+  return sequencesRecursive(root)
+}
+
+const sequencesRecursive = (node) => {
+  if (!node) return null
+
+  let perms = permutations(sequencesRecursive(node.left), sequencesRecursive(node.right))
+
+  if (!perms) perms = [[node.val]]
+
+  perms.forEach((permutation) => {
+    permutation.unshift(node.val)
+  })
+  return perms
+}
+
+const permutations = (left, right) => {
+  if (!left || !right) {
+    return left || right
+  }
+
+  let perms = []
+
+  for (let i = 0, len = left.length; i < len; i++) {
+    for (let j = 0, len = right.length; j < len; j++) {
+      perms.push.apply(perms, permutationsRecursive([], left[i], right[j], [], 0, 0))
+    }
+  }
+
+  return perms
+}
+
+const permutationsRecursive = (perms, list1, list2, prefix, i, j) => {
+  if (i === list1.length && j === list2.length) {
+    perms.push(prefix.slice(0))
+  } else {
+    if (i < list1.length) {
+      prefix.push(list2[i])
+      permutationsRecursive(perms, list1, list2, prefix, i + 1, j)
+      prefix.pop()
+    }
+
+    if (j < list2.length) {
+      prefix.push(list2[j])
+      permutationsRecursive(perms, list1, list2, prefix, i, j + 1)
+      prefix.pop()
+    }
+  }
+  return perms
+}
+
+console.log(bstSequences(bstWithParent))
--- Trees_Graphs/paths_with_sum.js
+++ Trees_Graphs/paths_with_sum.js
@@ -2,3 +2,124 @@
 // (which might be positive or negative). Design an algorithm to count the
 // number of paths that sum to a given vialue. The path does not need to start
 // or end at the root or a leaf, but it must go downwards (traveling from parent to child)
+
+
+// Clarifications:
+// If a single node hold the same value as the target sum, can that be considered a path?
+
+// Ideas:
+// Do a DFS on the tree starting at the root node and push the node values into a queue.
+// Keep a running total of the current path. If the sum equals the number, incriment the count.
+// When the path reaches a leaf node, shift the queue and subtract that number from the total.
+// If the total is hit, incriment the total. If it falls below the total return 0.
+// Return the total count
+
+// Complexities:
+  // Time: o(n)
+  // Space: o(n^2) <-- recursion stacks & queue space
+
+// const { BSTNode, bstWithParent } = require('./graph')
+// const pathsWithSum = (root, targetSum) => {
+//   const sumsDown = []
+//   let count = (
+//     pathSumsDown(root.left, sumsDown, targetSum)
+//    +
+//     pathSumsDown(root.right, sumsDown, targetSum)
+//  )
+//  console.log(sumsDown)
+//
+//  if (root.value === targetSum) count++
+//
+//  count += pathSumsUp(root, sumsDown, targetSum)
+//
+//  return count
+// }
+//
+// const pathSumsDown = (root, sumsDown, targetSum) => {
+//   const seenValues = new Set()
+//   let nodeStack = [root]
+//   let sum = 0
+//   let count = 0
+//
+//
+//   while (nodeStack.length > 0) {
+//     if (sum === targetSum) cout++
+//     const currentNode = nodeStack.pop()
+//
+//     if (!currentNode) {
+//       sumsDown.push(sum)
+//       sum -= sumsDown[sumsDown.length - 1]
+//     }
+//
+//     if (currentNode && !seenValues.has(currentNode.value)) {
+//       seenValues.add(currentNode.value)
+//       sum += currentNode.value
+//
+//       nodeStack.push(currentNode.left)
+//       nodeStack.push(currentNode.right)
+//     }
+//   }
+//
+//   return count
+// }
+//
+// const pathSumsUp = (root, sumsDown, targetSum) => {
+//   const seenValues = new Set()
+//   let nodeStack = [root.left, root.right]
+//   let sum = sumsDown[0]
+//   let count = 0
+//
+//   while (nodeStack.length > 0) {
+//     const currentNode = nodeStack.pop()
+//
+//     if (!currentNode) {
+//       sumsDown.shift()
+//       sum = sumsDown[0]
+//     }
+//
+//     if (currentNode && !seenValues.has(currentNode.value)) {
+//       seenValues.add(currentNode.value)
+//       sum -= currentNode.value
+//
+//       if (sum === targetSum) count++
+//
+//       nodeStack.push(currentNode.left)
+//       nodeStack.push(currentNode.right)
+//     }
+//
+//   }
+//
+//   return count
+// }
+
+const { BSTNode, bstWithParent } = require('./graph')
+
+const pathsWithSum = (tree, value) => {
+  if (!tree) {
+    throw new Error("Tree must be valid and non-empty")
+  }
+
+  return findPathWithSumRecursive([], tree, value)
+}
+
+const findPathWithSumRecursive = (path, node, value) => {
+  let count = 0
+  if (node) {
+    path.push(node.val)
+    let sum = 0
+    for (let i = path.length, len = 0; i > len; i--) {
+      sum += path[i]
+      if (sum === value) ++count
+    }
+    count += (
+      findPathWithSumRecursive(path, node.left, value)
+      +
+      findPathWithSumRecursive(path, node.right, value)
+    )
+    path.pop()
+  }
+  return count
+}
+
+// console.log(bstWithParent)
+console.log(pathsWithSum(bstWithParent, 104)) // 1
